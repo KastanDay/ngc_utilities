e86d6fe7d8d3d6f0fb7bdc98be86ade7e1f032f5
Kastan Day <kastanvday@gmail.com>
2022-09-02 01:58:01 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM nvcr.io/nvidia/pytorch:22.08-py3
---
+++ runfile.sh
@@ -0,0 +1,1 @@
+echo "BEST DOCKER PYTORCH COMMAND"; sudo docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 nvcr.io/nvidia/pytorch:22.08-py3 nvidia-smi
